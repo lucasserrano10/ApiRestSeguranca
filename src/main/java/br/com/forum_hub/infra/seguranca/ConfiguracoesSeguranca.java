c5e5013efa99372701464c5caa8dacbdfe7329c5
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2026-02-16 23:07:52 -0300

    roles

--- src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
@@ -2,6 +2,9 @@ package br.com.forum_hub.infra.seguranca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,6 +32,18 @@ public class ConfiguracoesSeguranca {
                         req ->
                         {
                             req.requestMatchers("/login", "/atualizar-token", "/registrar", "/verificar-token").permitAll();
+
+                            req.requestMatchers(HttpMethod.GET, "/cursos").permitAll();
+                            req.requestMatchers(HttpMethod.GET, "/topicos/**").permitAll();
+
+                            req.requestMatchers(HttpMethod.POST, "/topicos").hasRole("ESTUDANTE");
+                            req.requestMatchers(HttpMethod.PUT, "/topicos").hasRole("ESTUDANTE");
+                            req.requestMatchers(HttpMethod.DELETE, "/topicos/**").hasRole("ESTUDANTE");
+
+                            req.requestMatchers(HttpMethod.PATCH, "/topicos/**").hasRole("MODERADOR");
+
+                            req.requestMatchers(HttpMethod.PATCH, "/adicionar-perfil/**").hasRole("ADMIN");
+
                             req.anyRequest().authenticated();
                         }
                 )
@@ -46,6 +61,14 @@ public class ConfiguracoesSeguranca {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public RoleHierarchy hierarquiaPerfis(){
+        String hierarquia = "ROLE_ADMIN > ROLE_MODERADOR\n" +
+                "ROLE_MODERADOR > ROLE_INSTRUTOR\n" +
+                "ROLE_MODERADOR > ROLE_ESTUDANTE";
+        return RoleHierarchyImpl.fromHierarchy(hierarquia);
     }
 
 }
